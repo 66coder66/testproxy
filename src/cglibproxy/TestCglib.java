11aee25ef5d7c0c048a97c2f6106ba99104e4acc
66coder66 <390388252@qq.com>
2018-01-11 15:42:58 +0800

    junit.test

--- src/cglibproxy/TestCglib.java
+++ src/cglibproxy/TestCglib.java
@@ -5,18 +5,18 @@ import org.junit.Test;
 public class TestCglib {
 	
 	
-	public static void main(String[] args) {
-		BookImpl book = new BookImpl();
-		BookCglib  cglib = new BookCglib();  
-		BookImpl bookCglib = (BookImpl)cglib.getInstance(book);  
-        bookCglib.addBook(); 
-	}
+//	public static void main(String[] args) {
+//		BookImpl book = new BookImpl();
+//		BookCglib  cglib = new BookCglib();  
+//		BookImpl bookCglib = (BookImpl)cglib.getInstance(book);  
+//        bookCglib.addBook(); 
+//	}
 	
-	/*@Test  
+	@Test  
     public void testProxy() throws Throwable{  
 		BookImpl book = new BookImpl();
 		BookCglib  cglib = new BookCglib();  
 		BookImpl bookCglib = (BookImpl)cglib.getInstance(book);  
         bookCglib.addBook(); 
-    } */
+    } 
 }
